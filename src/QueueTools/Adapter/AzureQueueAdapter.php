1e62d16526d1893192a1f9cc45d111bac760b70f
Pascal Paulis <ppaulis@gmail.com>
2022-05-17 17:05:37 +0200

    Add azure php sdk

---
+++ src/QueueTools/Adapter/AzureQueueAdapter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BayWaReLusy\QueueTools\Adapter;
+
+use BayWaReLusy\QueueTools\Message;
+
+class AzureQueueAdapter implements PollingQueueAdapterInterface
+{
+
+
+    public function receiveMessage(string $queueUrl): ?Message
+    {
+        // TODO: Implement receiveMessage() method.
+    }
+
+    public function sendMessage(string $queueUrl, string $messageBody, string $messageGroupId = null, string $messageDeduplicationId = null): QueueAdapterInterface
+    {
+        // TODO: Implement sendMessage() method.
+    }
+
+    public function deleteMessage(string $queueUrl, Message $message): QueueAdapterInterface
+    {
+        // TODO: Implement deleteMessage() method.
+    }
+}
